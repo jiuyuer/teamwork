4e4608ac96951f8974845326dd554def79dabe7a
tonyt <jiuyue14@163.com>
2017-11-15 00:11:12 +0800

    add progress

--- src/views/taskList/index.vue
+++ src/views/taskList/index.vue
@@ -13,6 +13,16 @@
                     </section>
                 </div>
             </li>
+            <li class="task-list-con">
+                <a class="task-list-btn" @click="showPop" v-show="!progressPop"><i class="el-icon-plus"></i>新建任务列表</a>
+                <div class="task-list-box" v-show="progressPop">
+                    <el-input v-model.trim="progressName" @keyup.enter="savePop" placeholder="请输入内容"></el-input>
+                    <div class="buttons">
+                        <el-button @click="closePop" size="small">取消</el-button>
+                        <el-button @click="savePop" size="small" type="primary">保存</el-button>
+                    </div>
+                </div>
+            </li>
         </ul>
         <router-view></router-view>
     </div>
@@ -28,8 +38,66 @@ export default {
     return {
       progress: {
         data: []
-      }
+      },
+      progressName: "",
+      progressPop: false
     };
+  },
+  methods: {
+    showPop() {
+      this.progressPop = true;
+    },
+    closePop() {
+      this.progressPop = false;
+    },
+    savePop() {
+      if(this.progressName == ""){
+        this.$message({
+          showClose:true,
+          duration: 2000,
+          message: '不能为空！',
+          type: 'error'
+        });
+        return false;
+      }
+      let obj = {
+        progressName: this.progressName
+      };
+      this.progress.data.push(obj);
+      let $con = document.getElementById("project-content");
+      let $ul = $con.childNodes[0];
+      let len = $ul.childNodes.length;
+      let width_li = $ul.childNodes[0].clientWidth + 10;
+      this.$nextTick(() => {
+        $con.scrollLeft = width_li * len;
+      });
+    },
+    documentActive(event) {
+      let $li = this.closestElement(event.target, "li");
+      if ($li && $li.className == "task-list-con") {
+        event.preventDefault();
+        return false;
+      } else {
+        this.closePop();
+      }
+    },
+    //  Element.closest() 方法用来获取：匹配特定选择器且离当前元素最近的祖先元素（也可以是当前元素本身）。如果匹配不到，则返回 null。
+    //  https://developer.mozilla.org/zh-CN/docs/Web/API/Element/closest
+    closestElement(el, selector) {
+      var matchesSelector =
+        el.matches ||
+        el.webkitMatchesSelector ||
+        el.mozMatchesSelector ||
+        el.msMatchesSelector;
+
+      while (el) {
+        if (matchesSelector.call(el, selector)) {
+          break;
+        }
+        el = el.parentElement;
+      }
+      return el;
+    }
   },
   mounted() {
     http(apiSetting.getProgress)
@@ -54,15 +122,18 @@ export default {
           );
         });
       });
+    document.addEventListener("click", this.documentActive, false);
   }
 };
 </script>
 
 <style lang="less">
+@import "../../../static/global.less";
+
 #project-content {
   position: fixed;
   z-index: 0;
-  top: 73px;
+  top: 72px;
   bottom: 0;
   overflow-y: hidden;
   background: #fff;
@@ -74,6 +145,29 @@ export default {
     white-space: nowrap;
     margin: 0px;
     padding: 10px;
+    > li.task-list-con {
+      height: initial;
+      .task-list-btn {
+        font-size: @f-14;
+        color: #a6a6a6;
+        cursor: pointer;
+        padding: 17px 18px;
+        display: block;
+        i.el-icon-plus {
+          margin-right: 7px;
+        }
+        &:hover {
+          color: #66b1ff;
+        }
+      }
+      .task-list-box {
+        padding: 5px 5px 0px 5px;
+        .buttons {
+          padding: 8px 0;
+          text-align: right;
+        }
+      }
+    }
     > li {
       position: relative;
       width: 280px;
@@ -82,7 +176,6 @@ export default {
       display: inline-flex;
       flex-direction: column;
       align-items: stretch;
-      border: 1px solid #ccc;
       background: #eee;
       header {
         flex: 1 0 auto;
